ffbf4b306864b53167676e7fc8ea8f9bcab877dd
Inbisat <a23inbnavnav@inspedralbes.cat>
2025-01-24 10:47:57 +0100

    Define main page structure and WebTemplate component

--- front/src/app/components/Sidebar.js
+++ front/src/app/components/Sidebar.js
@@ -2,20 +2,20 @@ import React from "react";
 
 const Sidebar = () => {
   return (
-    <div className="bg-emerald-100 w-1/4 h-full p-4 border-r border-black-200">
+    <div className="bg-gray-200 dark:bg-gray-800 text-black dark:text-white w-1/4 h-full p-4 border-r border-gray-300 dark:border-gray-700">
       <div className="text-center mb-6">
-        <div className="w-16 h-16 rounded-full bg-gray-300 mx-auto mb-2"></div>
+        <div className="w-16 h-16 rounded-full bg-gray-300 dark:bg-gray-600 hover:bg-gray-400 dark:hover:bg-gray-500 mx-auto mb-2"></div>
         <h2 className="text-lg font-semibold">USER</h2>
-        <p className="text-sm text-gray-500">Admin</p>
+        <p className="text-sm text-gray-600 dark:text-gray-400">Admin</p>
       </div>
       <nav className="space-y-4">
-        <button className="w-full px-4 py-2 bg-amber-500 rounded-md hover:bg-gray-300 text-left">
+        <button className="w-full px-4 py-2 bg-gray-300 dark:bg-gray-600 rounded-md hover:bg-gray-400 dark:hover:bg-gray-500 text-left">
           📄 Languages
         </button>
-        <button className="w-full px-4 py-2 bg-blue-200 rounded-md hover:bg-gray-300 text-left">
+        <button className="w-full px-4 py-2 bg-gray-300 dark:bg-gray-600 rounded-md hover:bg-gray-400 dark:hover:bg-gray-500 text-left">
           🎮 Juegos
         </button>
-        <button className="w-full px-4 py-2 bg-pink-200 rounded-md hover:bg-gray-300 text-left">
+        <button className="w-full px-4 py-2 bg-gray-300 dark:bg-gray-600 rounded-md hover:bg-gray-400 dark:hover:bg-gray-500 text-left">
           ⚙️ Configuración
         </button>
       </nav>
--- front/src/app/page.js
+++ front/src/app/page.js
@@ -1,15 +1,18 @@
 import Sidebar from "./components/Sidebar";
-import Card from "./components/Card";
+import Navbar from "./components/Navbar";
 import ContentArea from "./components/ContentArea";
 
 
 
 const Page = () => {
   return (
-    <div className="flex h-screen bg-gray-50">
+    <div className="flex h-screen bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-gray-50">
       <Sidebar />
+      
+
       <div className="flex flex-col w-full">
-        <Card />
+        <Navbar />
+
         <ContentArea />
       </div>
     </div>
