2eb65619269dc5bcb8c235f66197a78dfdd8fb35
Inbisat <a23inbnavnav@inspedralbes.cat>
2025-01-27 20:30:29 +0100

    enroute of pages is working

--- front/src/app/page.js
+++ front/src/app/page.js
@@ -1,11 +1,32 @@
-import React from 'react';
-import Login from '../pages/Login'
+"use client"; // Mark this file as a client component
 
-const  Signup= () => {
+import React, { useState } from 'react';
+import StPage from '../pages/StPage'; 
+import Login from '../pages/Login'; 
+
+const Page = () => {
+  const [showLogin, setShowLogin] = useState(true);
+
   return (
-   <Login />
+    <div>
+      {showLogin ? <Login /> : <StPage />}
+
+      <div className="fixed bottom-4 right-4 flex gap-4">
+        <button
+          onClick={() => setShowLogin(true)}
+          className="px-4 py-2 bg-gray-500 text-white rounded"
+        >
+          Show Login
+        </button>
+        <button
+          onClick={() => setShowLogin(false)}
+          className="px-4 py-2 bg-gray-700 text-white rounded"
+        >
+          Show StPage
+        </button>
+      </div>
+    </div>
   );
 };
 
-export default Signup;
-
+export default Page;
