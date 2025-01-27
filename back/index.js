804fdd2b33145bc7e4a684306b61c90fe455340c
Arnau Fernandez Gil <a22arnfergil@inspedralbes.cat>
2025-01-27 09:48:44 +0100

    create class and join class working

--- back/index.js
+++ back/index.js
@@ -1,21 +1,29 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import mysql from 'mysql2/promise';
+import ShortUniqueId from 'short-unique-id';
 
+// Load environment variables from .env file
 dotenv.config();
 
+// Create an Express application
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
+// Parse JSON bodies for this app
 app.use(express.json());
 
-
+// Database configuration
 const dbConfig = {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_DATABASE
 };
+
+// Unique ID generator used in class code generation
+const uid = new ShortUniqueId({ length: 10 });
+
 
 async function createConnection() {
     try {
@@ -46,6 +54,8 @@ testConnection();
 app.post('/api/class', async (req, res) => {
     const { name, teacher_id } = req.body;
 
+    const language = "[]" /* TODO: Add language array to class creation */
+
     if (!name || !teacher_id) {
         return res.status(400).json({ error: 'Class name and teacher ID are required' });
     }
@@ -53,7 +63,7 @@ app.post('/api/class', async (req, res) => {
     try {
         const connection = await createConnection();
         const [rows] = await connection.execute(
-            'SELECT id FROM users WHERE googleId = ? AND teacher = "1"',
+            'SELECT id FROM USER WHERE id = ? AND teacher = "1"',
             [teacher_id]
         );
         await connection.end();
@@ -66,52 +76,63 @@ app.post('/api/class', async (req, res) => {
         return res.status(500).json({ error: 'Internal server error' });
     }
 
+    const class_code = uid.rnd();
+
+    console.log(class_code);
+
     try {
         const connection = await createConnection();
         const [result] = await connection.execute(
-            'INSERT INTO CLASS (name, teacher_id) VALUES (?, ?)',
-            [name, JSON.stringify([teacher_id])]
+            'INSERT INTO CLASS (name, teacher_id, language, code) VALUES (?, ?, ?, ?)',
+            [name, JSON.stringify([teacher_id]), language, class_code]
         );
         await connection.end();
 
-        res.status(201).json({ message: 'Class created successfully', classId: result.insertId });
+        res.status(201).json({ class_id: result.insertId, name, teacher_id, language, class_code});
     } catch (error) {
         console.error('Error creating class:', error);
         res.status(500).json({ error: 'Internal server error' });
     }
 });
 
-app.post('/api/join-student', async (req, res) => {
-    const { classCode, studentId } = req.body;
+app.post('/api/class/enroll', async (req, res) => {
+    const { class_code, user_id } = req.body;
 
-    if (!classCode || !studentId) {
+    if (!class_code || !user_id) {
         return res.status(400).json({ error: 'Class code and student ID are required' });
     }
 
     try {
         const connection = await createConnection();
         const [rows] = await connection.execute(
-            'SELECT id FROM CLASS WHERE code = ?',
-            [classCode]
+            'SELECT idclass FROM CLASS WHERE code = ?',
+            [class_code]
         );
         await connection.end();
 
         if (rows.length === 0) {
             return res.status(400).json({ error: 'Class does not exist' });
         } else {
-            const classId = rows[0].id;
+            const class_id = rows[0].idclass;
+            // const teacher_info = JSON.parse(rows[0].teacher_id);
+            // const language_info = JSON.parse(rows[0].language);
             try {
+
+                const { name, language_info, teacher_info, classmate_info } = await getClassInfo(class_id);
+
                 const connection = await createConnection();
                 const [result] = await connection.execute(
-                    'UPDATE USER SET class_id = ? WHERE googleId = ?',
-                    [classId, studentId]
+                    'UPDATE USER SET class = ? WHERE id = ?',
+                    [class_id, user_id]
                 );
                 await connection.end();
 
                 if (result.affectedRows === 0) {
-                    return res.status(400).json({ error: 'Student could not be added to class' });
+                    return res.status(400).json({ error: `Student could not be added to class, student doesn't exist` });
                 } else {
-                    res.json({ message: 'Student added to class successfully' });
+
+                    const class_details = { name, class_id, language_info, teacher_info, classmate_info };
+                    res.json({ message: 'Student has been successfully enrolled in the class', class_details  });
                 }
             } catch (error) {
                 console.error('Error adding student to class:', error);
@@ -123,6 +144,58 @@ app.post('/api/join-student', async (req, res) => {
         return res.status(500).json({ error: 'Internal server error' });
     }
 });
+
+function getClassInfo(class_id) {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const connection = await createConnection();
+            const [rows] = await connection.execute(
+                'SELECT name, language, teacher_id FROM CLASS WHERE idclass = ?',
+                [class_id]
+            );
+            await connection.end();
+
+            if (rows.length === 0) {
+                reject('Class does not exist');
+            } else {
+                const connection = await createConnection();
+                const [classmates] = await connection.execute(
+                    'SELECT id, name FROM USER WHERE class = ?',
+                    [class_id]
+                );
+                await connection.end();
+
+                const classmate_info = classmates.map(({ id, name }) => ({ id, name }));
+
+                const { name, language, teacher_id } = rows[0];
+
+                const parsed_teacher_id = JSON.parse(teacher_id);
+
+                console.log("ID professors: ", parsed_teacher_id);
+
+                const teacher_info = await Promise.all(parsed_teacher_id.map(async (id) => {
+                    const connection = await createConnection();
+                    const [teacher] = await connection.execute(
+                        'SELECT name FROM USER WHERE id = ?',
+                        [id]
+                    );
+                    await connection.end();
+
+                    console.log("Teacher info after SELECT: ", teacher[0]);
+
+                    return { id, name: teacher[0].name };
+                }));
+
+
+               const language_info = JSON.parse(language);
+
+                resolve({ class_id, name, language_info, teacher_info, classmate_info });
+            }
+        } catch (error) {
+            reject('Internal server error');
+        }
+    });
+}
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
