3e8424ccb34e171490670e53181e028f339951da
Inbisat <a23inbnavnav@inspedralbes.cat>
2025-01-28 09:08:34 +0100

    enroute JoinClass page in 'page.js'

--- front/src/app/page.js
+++ front/src/app/page.js
@@ -1,30 +1,53 @@
-"use client"; 
+"use client"
 
-import React, { useState } from 'react';
-import StPage from '../pages/StPage'; 
-import Login from '../pages/Login'; 
+import React, { useState } from "react"
+import StPage from "../pages/StPage";
+import Login from "../pages/Login";
+import JoinClass from "../pages/JoinClass";
+
 
 const Page = () => {
-  const [showLogin, setShowLogin] = useState(true);
+  const [currentPage, setCurrentPage ] = useState('login');
 
-  return (
+  const renderPage = () => {
+    switch(currentPage) {
+      case 'login':
+        return <Login />;
+        case 'stpage':
+          return <StPage />;
+          case 'joinclass':
+          return <JoinClass />;
+          default:
+            return <Login />;
+
+    }
+  }
+
+  return(
     <div>
-      {showLogin ? <Login /> : <StPage />}
+      {renderPage()}
 
       <div className="fixed bottom-4 right-4 flex gap-4">
+
         <button
-          onClick={() => setShowLogin(true)}
-          className="px-4 py-2 bg-gray-500 text-white rounded"
-        >
-          Show Login
+        onClick={() => setCurrentPage('login')}
+        className="px-4 py-2 bg-gray-400 text-white rounded">
+          Login
         </button>
+
         <button
-          onClick={() => setShowLogin(false)}
-          className="px-4 py-2 bg-gray-700 text-white rounded"
-        >
-          Show StPage
+        onClick={() => setCurrentPage('joinclass')}
+        className="px-4 py-2 bg-gray-500 text-white rounded">
+          joinclass
+        </button>
+
+        <button
+        onClick={() => setCurrentPage('stpage')}
+        className="px-4 py-2 bg-gray-600 text-white rounded">
+          Student Page
         </button>
       </div>
+
     </div>
   );
 };
