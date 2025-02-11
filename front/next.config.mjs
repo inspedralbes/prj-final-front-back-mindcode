9eeef442453cd129dae3505401866c529cf1b1a3
Arnau Fernandez Gil <a22arnfergil@inspedralbes.cat>
2025-02-11 12:09:48 +0100

    change config try fix source errors

--- front/next.config.mjs
+++ front/next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "standalone",
+    webpack(config, options) {
+        if (options.isServer) config.devtool = 'source-map';
+        return config;
+    },
 };
 
 export default nextConfig;
