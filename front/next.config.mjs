a28a0c9c90b766c993056cbf948b5b92c18565f9
Arnau Fernandez Gil <a22arnfergil@inspedralbes.cat>
2025-02-11 12:25:56 +0100

    change config again

--- front/next.config.mjs
+++ front/next.config.mjs
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "standalone",
-    webpack(config, options) {
-        if (options.isServer) config.devtool = 'source-map';
-        return config;
-    },
+    basePath: '',
+    assetPrefix: '/',
+    // webpack(config, options) {
+    //     if (options.isServer) config.devtool = 'source-map';
+    //     return config;
+    // },
 };
 
 export default nextConfig;
