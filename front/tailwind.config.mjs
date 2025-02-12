4c03bdc5ab938af2562bfb6aa82f737d205aeb5d
Arnau Fernandez Gil <a22arnfergil@inspedralbes.cat>
2025-02-12 10:19:30 +0100

    again

--- front/tailwind.config.mjs
+++ front/tailwind.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   important: true,
-  content: [
-    "./src/app/components/**/*.{js,ts,jsx,tsx,mdx}",
+  purge: [
+    // "./src/app/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/Login/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/JoinClass/*.{js,ts,jsx,tsx,mdx}",
+    // "./src/app/Login/*.{js,ts,jsx,tsx,mdx}",
+    // "./src/app/JoinClass/*.{js,ts,jsx,tsx,mdx}",
   ],
   darkMode: 'class', 
   theme: {
