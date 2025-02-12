ad8b5f566500690939adea51300116b9890846c6
Arnau Fernandez Gil <a22arnfergil@inspedralbes.cat>
2025-02-12 09:58:28 +0100

    slight change on tailwind config

--- front/tailwind.config.mjs
+++ front/tailwind.config.mjs
@@ -2,8 +2,7 @@
 export default {
   important: true,
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   darkMode: 'class', 
