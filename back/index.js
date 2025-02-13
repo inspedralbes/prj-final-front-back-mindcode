22336da22037d49275b7a2a0a5979ece8e44b93d
Arnau Fernandez Gil <a22arnfergil@inspedralbes.cat>
2025-02-13 12:44:58 +0100

    fix port back

--- back/index.js
+++ back/index.js
@@ -31,6 +31,7 @@ app.use(express.json());
 const dbConfig = {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
+    port: process.env.DB_PORT,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE
 };
